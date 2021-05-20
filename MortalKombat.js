12976135e3dd8946f4cca4431b78e96480f2798c
Boyer-Corentin <corentin.be8@gmail.com>
2021-05-20 12:19:03 +0200

    Exo

---
+++ MortalKombat.js
@@ -0,0 +1,88 @@
+// Exercice I
+// var tableau = [
+//   {
+//     id: 0,
+//     product: "shoes",
+//     price: 70,
+//   },
+//   {
+//     id: 1,
+//     product: "screen",
+//     price: 90,
+//   },
+//   {
+//     id: 2,
+//     product: "playstation V",
+//     price: 499,
+//   },
+//   {
+//     id: 3,
+//     product: "jean",
+//     price: 50,
+//   },
+// ];
+
+// Créer un code, qui va calculer le prix total de ces objets
+
+// var Result = 0;
+// for (var i = 0; i < tableau.length; i++) {
+//   Result += tableau[i].price;
+// }
+// console.log(Result);
+
+// Exercice II
+// Créer une fonction, qui va nous afficher l'objet le moins cher
+
+// function free(moins)
+// var Abs= 0
+// for (var i = 0; i < tableau[i].length; i++)
+// }
+
+// Exercice III
+// Créer une fonction qui va ajouter la clé 'stock' a tous les objets
+// Et ajoutera comme valeur, de façon aléatoire: true or false
+
+//   var rand = Math.random() >= 0.5);
+//   for (var i = 0; i < tableau.length; i++) {
+//     tableau[i].stock = [i];
+//   }
+//   console.log(tableau);
+// }
+// ajout();
+
+// Exercice IV
+// A l'aide de l'exercice précédent
+// Afficher dans le terminal les objets seulement en stock
+
+// ---------------------------------------------------------------------
+
+// var musics = [
+//   {
+//     id: 0,
+//     group: "Muse",
+//     song: "Starlight",
+//     rate: "5 / 10",
+//   },
+//   {
+//     id: 1,
+//     group: "Spiderbait",
+//     song: "Black Betty",
+//     rate: "9 / 10",
+//   },
+//   {
+//     id: 2,
+//     group: "Franz Ferdinand",
+//     song: "Take me out",
+//     rate: "7.5 / 10",
+//   },
+// ];
+
+// Exercice V
+// A l'aide du tableau musics
+// Créer une fonction qui va choisir un objet de façon aléatoire
+// Puis lui ajouter la clé "award: true"
+
+// Exercice VI
+// A l'aide du tableau musics
+// Afficher dans le terminal la musique la mieux notée
+// Indices : ".split()" et "parseInt()"
